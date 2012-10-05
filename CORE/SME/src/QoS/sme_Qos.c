91e68ce112fd12816f73bfc74f4f5e63b98c20cc
Madan Mohan Koyyalamudi <c_mkoyya@qca.qualcomm.com>
2012-10-05 14:56:51 -0700

    wlan: Decreasing the priority of unnecessary logs observed during loading/unloading wlan module
    Removed unnecessary logs which were observed when starting/turning off
    wifi.
    
    Change-Id: I9fdbf75a2cf31a16c5e402ab1bda978648cbd83d
    CR-Fixed: NA
    
    Conflicts:
    
    	prima/CORE/SME/src/csr/csrApiRoam.c
    	prima/CORE/WDI/CP/src/wlan_qct_wdi.c

--- CORE/SME/src/QoS/sme_Qos.c
+++ CORE/SME/src/QoS/sme_Qos.c
@@ -5656,7 +5656,7 @@ static eHalStatus sme_QosDeleteExistingFlows(tpAniSirGlobal pMac,
    pEntry = csrLLPeekHead( &sme_QosCb.flow_list, VOS_TRUE );
    if(!pEntry)
    {
-      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
+      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_WARN,
                 "%s: %d: Flow List empty, nothing to delete",
                 __FUNCTION__, __LINE__);
       return eHAL_STATUS_FAILURE;
@@ -5876,7 +5876,7 @@ static eHalStatus sme_QosDeleteBufferedRequests(tpAniSirGlobal pMac,
    pEntry = csrLLPeekHead( &pSession->bufferedCommandList, VOS_TRUE );
    if(!pEntry)
    {
-      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR, 
+      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_WARN, 
                 "%s: %d: Buffered List empty, nothing to delete on session %d",
                 __FUNCTION__, __LINE__,
                 sessionId);
@@ -5885,7 +5885,7 @@ static eHalStatus sme_QosDeleteBufferedRequests(tpAniSirGlobal pMac,
    while( pEntry )
    {
       pNextEntry = csrLLNext( &pSession->bufferedCommandList, pEntry, VOS_TRUE );
-      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR, 
+      VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO, 
                 "%s: %d: deleting entry from buffered List",
                 __FUNCTION__, __LINE__);
       //delete the entry from Flow List
