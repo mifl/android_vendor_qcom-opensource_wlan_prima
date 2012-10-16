256f301c3ba5d67ed3bccc3c4691a295229f402a
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-10-16 13:48:13 -0700

    Prevent P2P from flushing scan results while roaming
    If we are in the process of roaming, don't allow P2P to flush the
    current scan results.  Otherwise the roaming will fail.

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -4353,7 +4353,7 @@ tANI_BOOLEAN csrRoamIsCCXAssoc(tpAniSirGlobal pMac)
 tANI_BOOLEAN csrRoamIsFastRoamEnabled(tpAniSirGlobal pMac)
 {
     return (pMac->roam.configParam.isFastRoamIniFeatureEnabled &&
-            (!csrIsConcurrentInfraConnected(pMac)));
+            (!csrIsConcurrentSessionRunning(pMac)));
 }
 #endif
 
