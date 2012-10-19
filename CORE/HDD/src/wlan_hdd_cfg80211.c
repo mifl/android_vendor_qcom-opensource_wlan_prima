c3f892c236e2902d516b9076b49be5a5ec0772c6
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-10-19 09:17:28 -0700

    Set scan_type to P2P_DISCOVERY only for social channel scan
    P2P scans were observed to be scanning invalid channels.  Set scan
    type to P2P_DISCOVERY only if it is a p2p search on social channel.

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -3769,6 +3769,8 @@ int wlan_hdd_cfg80211_scan( struct wiphy *wiphy, struct net_device *dev,
                          pScanInfo->flushP2pScanResults = 1;
                          sme_ScanFlushP2PResult( WLAN_HDD_GET_HAL_CTX(pAdapter),
                                           sessionId );
+                         /* set requestType to P2P Discovery */
+                         scanRequest.requestType = eCSR_SCAN_P2P_DISCOVERY;
                     }
 
                     /*
@@ -3784,8 +3786,6 @@ int wlan_hdd_cfg80211_scan( struct wiphy *wiphy, struct net_device *dev,
                        scanRequest.skipDfsChnlInP2pSearch = 0;
                     }
 
-                    /* set requestType to P2P Discovery */
-                    scanRequest.requestType = eCSR_SCAN_P2P_DISCOVERY;
                 }
             }
 #endif
