56f487e5e2231f3f11e4f51dd90cddeeafac51b4
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-11-01 10:25:57 -0700

    Update MaxTxPower in join request to hal
    Fixes: Incorrect maxTxpower in join request to hal

--- CORE/WDI/CP/src/wlan_qct_wdi.c
+++ CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -7163,7 +7163,10 @@ WDI_ProcessBSSSessionJoinReq
 
   halJoinReqMsg.joinReqParams.linkState = pwdiJoinParams->wdiReqInfo.linkState;
 
-#ifndef WLAN_FEATURE_VOWIFI
+#ifdef WLAN_FEATURE_VOWIFI
+  halJoinReqMsg.joinReqParams.maxTxPower =
+    pwdiJoinParams->wdiReqInfo.wdiChannelInfo.cMaxTxPower;
+#else
   halJoinReqMsg.joinReqParams.ucLocalPowerConstraint =
     pwdiJoinParams->wdiReqInfo.wdiChannelInfo.ucLocalPowerConstraint;
 #endif
