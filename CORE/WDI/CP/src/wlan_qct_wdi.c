fca1458c2ce6ccb05fb9ae2184d123aa22b563c3
Jeff Johnson <jjohnson@qca.qualcomm.com>
2012-11-01 10:31:26 -0700

    Fix for Memory Trailer Corruption
    While doing the memcpy instead of using the size of the union, taking
    the coresponding size based on the version

--- CORE/WDI/CP/src/wlan_qct_wdi.c
+++ CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -10434,10 +10434,10 @@ WDI_ProcessConfigStaReq
 
   /* Need to fill in the BSS index */
   halConfigStaReqMsg.uStaParams.configStaParams.bssIdx = pBSSSes->ucBSSIdx;
-
+  
   wpalMemoryCopy( pSendBuffer+usDataOffset,
                   &halConfigStaReqMsg.uStaParams,
-                  sizeof(halConfigStaReqMsg.uStaParams));
+                  uMsgSize);
 
   pWDICtx->wdiReqStatusCB     = pwdiConfigSTAParams->wdiReqStatusCB;
   pWDICtx->pReqStatusUserData = pwdiConfigSTAParams->pUserData;
