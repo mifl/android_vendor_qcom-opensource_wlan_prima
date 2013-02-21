8512e26bc685b3347356e1b7f7fbc7c32bbb500f
Jeff Johnson <jjohnson@qca.qualcomm.com>
2013-02-21 10:51:47 -0800

    wlan: Release 3.2.2.1
    Initial release of JB MR2 development branch, based upon mainline
    v3.2.0.56, with bbaebbe94ff496db1e8cd9b7c597b82f0beff83b reverted.

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -58,11 +58,11 @@ BRIEF DESCRIPTION:
 
 #define QWLAN_VERSION_MAJOR            3
 #define QWLAN_VERSION_MINOR            2
-#define QWLAN_VERSION_PATCH            0
+#define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            56
+#define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "3.2.0.56"
+#define QWLAN_VERSIONSTR               "3.2.2.1"
 
 #endif /* QWLAN_VERSION_H */
 
