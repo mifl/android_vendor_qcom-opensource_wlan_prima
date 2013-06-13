c883f1356d2f51888aba02e3fbd50f90b5744189
Bansidhar Gopalachari <bgopalac@qca.qualcomm.com>
2013-06-13 13:32:31 -0700

    wlan: Release 3.2.2.19
    wlan: Release 3.2.2.19

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            2
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            18
+#define QWLAN_VERSION_BUILD            19
 
-#define QWLAN_VERSIONSTR               "3.2.2.18"
+#define QWLAN_VERSIONSTR               "3.2.2.19"
 
 #endif /* QWLAN_VERSION_H */
 
