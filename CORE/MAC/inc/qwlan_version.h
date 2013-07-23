35734cdb9562defb2c5c0f9322a2c001fc4faa3b
Vinayak <vkamat@codeaurora.org>
2013-07-23 18:22:16 +0530

    wlan:Revision 3.2.4.136
    wlan:Revision 3.2.4.136

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            4
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            134
+#define QWLAN_VERSION_BUILD            136
 
-#define QWLAN_VERSIONSTR              "3.2.4.134"
+#define QWLAN_VERSIONSTR              "3.2.4.136"
 
 #endif /* QWLAN_VERSION_H */
 
