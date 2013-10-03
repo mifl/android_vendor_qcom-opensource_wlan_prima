596736054f9cbdc707018832e10d259d7d652507
pratik Bhalgat <pratik_bit@codeaurora.org>
2013-10-03 11:18:58 +0530

    wlan: Revision 3.2.6.115
    Revision 3.2.6.115
    
    Change-Id: Id26a1235ab830af07bcf3e8faa2163d578ceb413

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            114
+#define QWLAN_VERSION_BUILD            115
 
-#define QWLAN_VERSIONSTR              "3.2.6.114"
+#define QWLAN_VERSIONSTR              "3.2.6.115"
 
 #endif /* QWLAN_VERSION_H */
 
