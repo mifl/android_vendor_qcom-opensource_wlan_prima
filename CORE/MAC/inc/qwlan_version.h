2959e917b46474ce41517d5cb36404e9419f860e
pratik Bhalgat <pratik_bit@codeaurora.org>
2013-10-17 17:27:31 +0530

    wlan : Revision 3.2.6.120
    Revision 3.2.6.120
    
    Change-Id: I1222d00763fe414bb47b888c5b6ac7100d3b42aa

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -60,9 +60,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            119
+#define QWLAN_VERSION_BUILD            120
 
-#define QWLAN_VERSIONSTR              "3.2.6.119"
+#define QWLAN_VERSIONSTR              "3.2.6.120"
 
 #endif /* QWLAN_VERSION_H */
 
