af56c1733c2663d0c34c8d7da33eb0c265518a3a
Rashmi Ramanna <rashmi@codeaurora.org>
2013-10-22 12:36:36 +0530

    wlan: Place DHCP packets in VOICE queue on low resource condition.
    The DHCP packets are transmitted in best effort access category
    queues. When the queue is full, the dhcp renewel packets are
    dropped resulting connection failure.
    To avoid this, the DHCP packets are placed in VOICE queue if
    best effort access category queue hits the low resource condition.
    
    Change-Id: I49c621287238a809e9c3999f843bec492c7d9717
    CRs-fixed: 534950

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -586,6 +586,9 @@ typedef struct {
    /**Track whether OS TX queue has been disabled.*/
    v_BOOL_t txSuspended[NUM_TX_QUEUES];
 
+   /**Track whether 3/4th of resources are used on softAP/P2P GO side. */
+   v_BOOL_t vosLowResource;
+
    /** Track QoS status of station */
    v_BOOL_t isQosEnabled;
 
@@ -767,6 +770,9 @@ struct hdd_adapter_s
    hdd_list_t wmm_tx_queue[NUM_TX_QUEUES];
    /**Track whether VOS is in a low resource state*/
    v_BOOL_t isVosOutOfResource;
+
+   /**Track whether 3/4th of resources are used on STA/p2p client side */
+   v_BOOL_t isVosLowResource;
   
    /**Track whether OS TX queue has been disabled.*/
    v_BOOL_t isTxSuspended[NUM_TX_QUEUES];
