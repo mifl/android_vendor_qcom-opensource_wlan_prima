625a3f9f5f521ccb0f568b0cfea931126a63a174
Kiet Lam <kietl@codeaurora.org>
2013-11-12 19:17:47 +0530

    wlan: We were dereferencing pMac pointer to print debug when it was NULL.
    Instead of printing the debug message using smsLog (which use
    pMac as input) we are now using VOS_TRACE to print the debug
    message to avoid NULL pointer dereferencing.
    
    Change-Id: I36ecacaa46e664d8ad6cd08f294fcfec9024a8c7
    CRs-fixed: 573574

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -9322,7 +9322,8 @@ void smeGetCommandQStatus( tHalHandle hHal )
 
     if (NULL == pMac)
     {
-        smsLog( pMac, LOGE, "smeGetCommandQStatus: pMac is NULL" );
+        VOS_TRACE( VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_ERROR,
+                       "smeGetCommandQStatus: pMac is NULL");
         return;
     }
 
