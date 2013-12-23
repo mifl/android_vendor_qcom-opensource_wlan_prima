34a46887335bdadd1419068128ea0f90dd3cb5ed
Praveen Kumar Sirisilla <c_pksiri@qca.qualcomm.com>
2013-12-22 16:35:05 -0800

    Make sure ACKs are always sent after receiving a Probe Response
    Previously during active scan, STA will send 5 probe requests
    at 0th,10th,20th,30th,40th msec of scan interval.But with MAX
    channel timeout at 40th msec, STA after sending probe request,
    immediately switches the channel resulting in unacked probe
    response.This patch makes sure STA will not send probe request
    at MAX channel time slot.Also a dump command to configure num-
    ber of probe request is provided.
    
    Change-Id: I3bd9cb917662e8bffcff1b0d5f6a26ed83383de3
    CRs-fixed: 507103

--- CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
+++ CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
@@ -3582,6 +3582,7 @@ void limProcessInitScanRsp(tpAniSirGlobal pMac,  void *body)
                  */
                 pMac->lim.gLimNumOfConsecutiveBkgndScanFailure = 0;
                 pMac->lim.gLimNumOfBackgroundScanSuccess += 1;
+                pMac->lim.probeCounter = 0;
             }
             limContinueChannelScan(pMac);
             break;
