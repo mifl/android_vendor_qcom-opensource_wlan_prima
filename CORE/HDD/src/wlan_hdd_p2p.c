0cb982eaa872fab47ec9b89f9df01c19af18201a
Rashmi Ramanna <c_ramanr@qti.qualcomm.com>
2013-12-30 21:49:48 +0530

    P2P: Abort ongoing scan when connection is initiated.
    When DUT receives Provision Discovery request or
    GO negotiation request to initiate p2p connection,
    respective action frame's response will be delayed
    by wpa_supplicant, if p2p scanning is in progress.
    This results in multiple Provision discovery request
    retries.
    
    So, abort ongoing scan to remove delay in sending
    Provision Discovery response or GO negotiation
    response.
    
    Change-Id: I8c099438925f50647aad7654d7630fefd0f399f1
    CRs-Fixed: 576285

--- CORE/HDD/src/wlan_hdd_p2p.c
+++ CORE/HDD/src/wlan_hdd_p2p.c
@@ -1396,7 +1396,7 @@ void hdd_sendMgmtFrameOverMonitorIface( hdd_adapter_t *pMonAdapter,
          hddLog( LOG1, FL("Success"));
      }
      else
-         hddLog( LOGE, FL("Failed %d"), rxstat);                   
+         hddLog( LOGE, FL("Failed %d"), rxstat);
 
      return ;
 }
@@ -1413,6 +1413,8 @@ void hdd_indicateMgmtFrame( hdd_adapter_t *pAdapter,
     tANI_U8 subType = 0;
     tActionFrmType actionFrmType;
     hdd_cfg80211_state_t *cfgState = NULL;
+    hdd_context_t *pHddCtx = NULL;
+    hdd_scaninfo_t *pScanInfo = NULL;
 
     hddLog(VOS_TRACE_LEVEL_INFO, "%s: Frame Type = %d Frame Length = %d\n",
             __func__, frameType, nFrameLength);
@@ -1469,6 +1471,13 @@ void hdd_indicateMgmtFrame( hdd_adapter_t *pAdapter,
     {
         hddLog( LOGE, FL("pAdapter has invalid magic"));
         return;
+    }
+
+    pHddCtx = WLAN_HDD_GET_CTX(pAdapter);
+    if (NULL == pHddCtx)
+    {
+         hddLog(VOS_TRACE_LEVEL_FATAL,"%s: HDD Context Null Pointer", __func__);
+         return;
     }
 
     if ((WLAN_HDD_SOFTAP == pAdapter->device_mode) ||
@@ -1546,7 +1555,17 @@ void hdd_indicateMgmtFrame( hdd_adapter_t *pAdapter,
                     }
                 }
 #endif
-
+                if( (actionFrmType == WLAN_HDD_PROV_DIS_REQ) ||
+                    (actionFrmType == WLAN_HDD_GO_NEG_REQ) )
+                {
+                    pScanInfo =  &pHddCtx->scan_info;
+                    if((pScanInfo != NULL) && (pHddCtx->scan_info.mScanPending))
+                    {
+                        hddLog(LOGE,"Action frame received when Scanning is in"
+                                    " progress. Abort Scan.");
+                        hdd_abort_mac_scan(pAdapter->pHddCtx);
+                    }
+                }
                 if (((actionFrmType == WLAN_HDD_PROV_DIS_RESP) &&
                             (cfgState->actionFrmState == HDD_PD_REQ_ACK_PENDING)) ||
                         ((actionFrmType == WLAN_HDD_GO_NEG_RESP) &&
