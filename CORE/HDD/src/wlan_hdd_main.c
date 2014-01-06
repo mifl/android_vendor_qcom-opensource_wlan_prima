3248feda7e7f5252b1d7ac6518eac539cbe5813e
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-01-06 19:19:06 +0530

    Block scan during BTCOEX operations.
    Framework issues BTCOEXMODE 1/2 signifying the critical period
    where the WiFi has to acquire the antenna during the coex
    operations with BT. Thus use the same indication to also
    block off channel operations , VIZ scan.
    
    Change-Id: I82bcbc52408fb42a6b37a4480bab0e6a7aa64be9
    CRs-Fixed: 584636

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -1988,6 +1988,26 @@ int hdd_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
                    pAdapter->sessionId);
        }
 #endif
+       else if (strncmp(command, "BTCOEXMODE", 10) == 0 )
+       {
+           char *dhcpPhase;
+
+           dhcpPhase = command + 11;
+           if ('1' == *dhcpPhase)
+           {
+               VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
+                         FL("BTCOEXMODE %d"), *dhcpPhase);
+
+               pHddCtx->btCoexModeSet = TRUE;
+            }
+            else if ('2' == *dhcpPhase)
+            {
+                VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
+                         FL("BTCOEXMODE %d"), *dhcpPhase);
+
+               pHddCtx->btCoexModeSet = FALSE;
+            }
+       }
        else {
            hddLog( VOS_TRACE_LEVEL_WARN, "%s: Unsupported GUI command %s",
                    __func__, command);
