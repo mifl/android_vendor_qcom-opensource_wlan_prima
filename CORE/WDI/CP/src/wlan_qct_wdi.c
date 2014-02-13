86cc71e82b6ec5f731b98ce40d55e0fde73728e5
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-02-13 21:52:40 +0530

    WLAN: Fix BTCOEXMODE event processing.
    While processing BTOCEXMODE, passing BSSID to firmware, if session
    is valid, and as part of filling wdiRequest for WDI_DHCP_START_IND
    and WDI_DHCP_STOP_IND requets, eariler incorrect data size
    (uevetDataSize) was sent, now correcting it by seding size of
    'WDI_DHCPInd' structure.
    
    Change-Id: Ib8a349b1be1fc8cf725618cce62f214e0dfaaa98
    CRs-Fixed: 615603

--- CORE/WDI/CP/src/wlan_qct_wdi.c
+++ CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -26711,7 +26711,7 @@ WDI_dhcpStartInd
 
    wdiEventData.wdiRequest      = WDI_DHCP_START_IND;
    wdiEventData.pEventData      = wdiDHCPInd;
-   wdiEventData.uEventDataSize  = sizeof(wdiDHCPInd);
+   wdiEventData.uEventDataSize  = sizeof(WDI_DHCPInd);
    wdiEventData.pCBfnc          = NULL;
    wdiEventData.pUserData       = NULL;
 
@@ -26749,7 +26749,7 @@ WDI_dhcpStopInd
 
    wdiEventData.wdiRequest      = WDI_DHCP_STOP_IND;
    wdiEventData.pEventData      = wdiDHCPInd;
-   wdiEventData.uEventDataSize  = sizeof(wdiDHCPInd);
+   wdiEventData.uEventDataSize  = sizeof(WDI_DHCPInd);
    wdiEventData.pCBfnc          = NULL;
    wdiEventData.pUserData       = NULL;
 
