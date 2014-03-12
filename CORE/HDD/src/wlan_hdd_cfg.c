51173dc118185f9d7214840171f65e78fdd7157f
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-03-12 21:44:28 +0530

    wlan: Add ini parametr to configure dwell time for first time
    Add ini parameter, gIntialDwellTime, to configure dwell time
    for the first time. By updating this parameter, we are configuring
    maxChnTime(max channel time).
    
    By default this parameter is set to 0, meaning maxChnTime will
    be set to default value.
    
    Change-Id: I53f6e1a946c7e186a2fa288c9b713260cf0741b7
    CRs-Fixed: 626669

--- CORE/HDD/src/wlan_hdd_cfg.c
+++ CORE/HDD/src/wlan_hdd_cfg.c
@@ -2945,6 +2945,13 @@ REG_VARIABLE( CFG_TDLS_EXTERNAL_CONTROL, WLAN_PARAM_Integer,
                 hdd_config_t, overrideCountryCode,
                 VAR_FLAGS_OPTIONAL,
                (void *)CFG_OVERRIDE_COUNTRY_CODE_DEFAULT),
+
+   REG_VARIABLE(CFG_INITIAL_DWELL_TIME_NAME, WLAN_PARAM_Integer,
+               hdd_config_t, nInitialDwellTime,
+               VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+               CFG_INITIAL_DWELL_TIME_DEFAULT,
+               CFG_INITIAL_DWELL_TIME_MIN,
+               CFG_INITIAL_DWELL_TIME_MAX ),
 };
 
 
@@ -4688,6 +4695,7 @@ VOS_STATUS hdd_set_sme_config( hdd_context_t *pHddCtx )
    smeConfig.csrConfig.vccUlMacLossThreshold    = pConfig->nVccUlMacLossThreshold;
    smeConfig.csrConfig.nRoamingTime             = pConfig->nRoamingTime;
    smeConfig.csrConfig.IsIdleScanEnabled        = pConfig->nEnableIdleScan;
+   smeConfig.csrConfig.nInitialDwellTime        = pConfig->nInitialDwellTime;
    smeConfig.csrConfig.nActiveMaxChnTime        = pConfig->nActiveMaxChnTime;
    smeConfig.csrConfig.nActiveMinChnTime        = pConfig->nActiveMinChnTime;
    smeConfig.csrConfig.nPassiveMaxChnTime       = pConfig->nPassiveMaxChnTime;
