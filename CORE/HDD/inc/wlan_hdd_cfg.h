ba3175c808d31e430ba144be1c8cc65caa5be80d
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-03-14 13:57:35 +0530

    wlan: adding cfg.ini parameter to configure to RA filter.
    Add ini  parameters gRAFilterEnable and gRARateLimitInterval to
    enable/disable RA filter and to configure RA_RATELIMIT_INTERVAL
    respectively.
    
    Change-Id: I6b68f7c20ae4d61f8cbafe7f7aa3c21a6c5b0579
    CRs-Fixed: 621074.

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2050,6 +2050,16 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_ADVERTISE_CONCURRENT_OPERATION_MIN     ( 0 )
 #define CFG_ADVERTISE_CONCURRENT_OPERATION_MAX     ( 1 )
 
+#define CFG_RA_FILTER_ENABLE_NAME               "gRAFilterEnable"
+#define CFG_RA_FILTER_ENABLE_DEFAULT            (1)
+#define CFG_RA_FILTER_ENABLE_MIN                (0)
+#define CFG_RA_FILTER_ENABLE_MAX                (1)
+
+#define CFG_RA_RATE_LIMIT_INTERVAL_NAME         "gRARateLimitInterval"
+#define CFG_RA_RATE_LIMIT_INTERVAL_DEFAULT      (60)
+#define CFG_RA_RATE_LIMIT_INTERVAL_MIN          (0)
+#define CFG_RA_RATE_LIMIT_INTERVAL_MAX          (60)
+
 /*--------------------------------------------------------------------------- 
   Type declarations
   -------------------------------------------------------------------------*/ 
@@ -2475,6 +2485,8 @@ typedef struct
    v_BOOL_t                    gEnableStrictRegulatoryForFCC;
    eHddDot11Mode               sapDot11Mode;
    v_BOOL_t                    advertiseConcurrentOperation;
+   v_BOOL_t                    cfgRAFilterEnable;
+   v_U16_t                     cfgRARateLimitInterval;
 } hdd_config_t;
 /*--------------------------------------------------------------------------- 
   Function declarations and documenation
