58d7fc9a55594a62556421f2c989d1af1978faa2
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-05-08 00:13:57 +0530

    wlan: report correct max link-speed to UI.
    Populate rate-flags as per assoc response frame and use those
    flags when ini parameter 'gReportMaxLinkSpeed' is set to
    eHDD_LINK_SPEED_REPORT_MAX.
    
    Change-Id: I316fbb82c8dd1778dfed8c5125e076aa2cf49382
    CRs-Fixed: 637266

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -1042,6 +1042,7 @@ struct hdd_adapter_s
    /* UAPSD psb value configured through framework */
    v_U8_t configuredPsb;
    v_BOOL_t is_roc_inprogress;
+   v_U32_t maxRateFlags;
 };
 
 #define WLAN_HDD_GET_STATION_CTX_PTR(pAdapter) (&(pAdapter)->sessionCtx.station)
