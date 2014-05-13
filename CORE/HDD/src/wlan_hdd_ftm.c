fd27b283dc418f667590914dbe02f60c3166fd6b
Zhaoming Shu <zshu@qca.qualcomm.com>
2014-05-13 12:59:43 +0530

    wlan: Set channel properly when enable channel bonding
    The ioctl to set channel bonding was Setting the channel
    statically. But if channel bonding is not 0, the channel
    should be set according to bonding mode.
    
    Change-Id: I2e2810fba2598feae171235b2780219a39e9b9ce
    CRs-Fixed: 565729

--- CORE/HDD/src/wlan_hdd_ftm.c
+++ CORE/HDD/src/wlan_hdd_ftm.c
@@ -2773,6 +2773,38 @@ static VOS_STATUS wlan_ftm_priv_start_stop_ftm(hdd_adapter_t *pAdapter,
     return VOS_STATUS_SUCCESS;
 }
 
+static unsigned int get_primary_channel(unsigned int center_channel,unsigned int cb)
+{
+    unsigned int primary_channel = center_channel;
+
+    if (center_channel <= 14)
+        return primary_channel ;
+
+    switch (cb)
+    {
+        case PHY_DOUBLE_CHANNEL_LOW_PRIMARY:
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_LOW_40MHZ_CENTERED:
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_HIGH_40MHZ_LOW:
+            primary_channel -= 2;
+            break;
+
+        case PHY_DOUBLE_CHANNEL_HIGH_PRIMARY:
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_HIGH_40MHZ_CENTERED:
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_LOW_40MHZ_HIGH:
+            primary_channel += 2;
+            break;
+
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_LOW_40MHZ_LOW:
+            primary_channel -= 6;
+            break;
+
+        case PHY_QUADRUPLE_CHANNEL_20MHZ_HIGH_40MHZ_HIGH:
+            primary_channel += 6;
+            break;
+    }
+
+    return primary_channel;
+}
 
 static VOS_STATUS validate_channel(unsigned int channel,unsigned int cb)
 {
@@ -2843,7 +2875,7 @@ static VOS_STATUS wlan_ftm_priv_set_channel(hdd_adapter_t *pAdapter,v_U16_t chan
 
     pMsgBody = &pMsgBuf->msgBody;
 
-    pMsgBody->SetChannel.chId = channel;
+    pMsgBody->SetChannel.chId = get_primary_channel(channel, ftm_status.cbmode);
 
     VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Channel =%d\n",pMsgBody->SetChannel.chId);
     pMsgBody->SetChannel.cbState = ftm_status.cbmode ;
