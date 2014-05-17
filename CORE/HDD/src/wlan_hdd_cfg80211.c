d8387d629355f943f3d54edbcfacec84db2d0c97
c_hpothu <c_hpothu@qti.qualcomm.com>
2014-05-17 17:01:48 +0530

    wlan: HDD: Fix for issues reported by Static Analysis
    In wlan_hdd_cfg80211_get_key() using 'params.cipher' variable in
    MTRACE before variable is initialized. So, call MTRACE after
    'params.cipher' is populated.
    
    Change-Id: I6e97fd5801c776e4b7851a37dad4d507014c0232
    CRs-Fixed: 668667.

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -3936,9 +3936,6 @@ static int wlan_hdd_cfg80211_get_key(
 
     ENTER();
 
-    MTRACE(vos_trace(VOS_MODULE_ID_HDD,
-                     TRACE_CODE_HDD_CFG80211_GET_KEY,
-                     pAdapter->sessionId, params.cipher));
     hddLog(VOS_TRACE_LEVEL_INFO, "%s: device_mode = %d",
                                  __func__,pAdapter->device_mode);
 
@@ -3978,6 +3975,10 @@ static int wlan_hdd_cfg80211_get_key(
             params.cipher = IW_AUTH_CIPHER_NONE;
             break;
     }
+
+    MTRACE(vos_trace(VOS_MODULE_ID_HDD,
+                     TRACE_CODE_HDD_CFG80211_GET_KEY,
+                     pAdapter->sessionId, params.cipher));
 
     params.key_len = pRoamProfile->Keys.KeyLength[key_index];
     params.seq_len = 0;
