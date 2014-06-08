f0d506bc1e902f02f9094c85f7474658a0b064c9
Agarwal Ashish <ashishka@qti.qualcomm.com>
2014-06-08 15:25:23 +0530

    wlan: Reset CounrtyCode11d if CountryCodePriority is set.
    If gCountryCodePriotity is set to 1 and last given country is through
    11d, countryCode11d needs to reset to 00 so that when next time driver
    is being reloaded driver knows about correct county code.
    Instead 00 countrycode if driver pass xx, kernel won't be able to
    find XX in db.txt and it will take 3 sec to set countrycode as 00.
    
    CRs-Fixed: 676496
    Change-Id: Iad5a1d999652fd2ba1357a8d8da399328d59b430

--- CORE/SME/src/sme_common/sme_Api.c
+++ CORE/SME/src/sme_common/sme_Api.c
@@ -10318,3 +10318,17 @@ eHalStatus sme_UpdateConnectDebug(tHalHandle hHal, tANI_U32 set_value)
     return (status);
 }
 
+tANI_BOOLEAN  sme_Is11dCountrycode(tHalHandle hHal)
+{
+    tpAniSirGlobal pMac = PMAC_STRUCT( hHal );
+
+    if (VOS_TRUE == vos_mem_compare(pMac->scan.countryCodeCurrent,
+                  pMac->scan.countryCode11d, 2))
+    {
+        return eANI_BOOLEAN_TRUE;
+    }
+    else
+    {
+        return eANI_BOOLEAN_FALSE;
+    }
+}
