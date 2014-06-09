f71d57a45ced9ec9e99b0bdd70134c99b3b0f76d
Mihir Shete <smihir@qti.qualcomm.com>
2014-06-09 17:22:55 +0530

    wlan: fix race during device suspend
    While suspending the device, driver notifies
    its threads about the system's intentions and
    waits for the threads to process its pending
    tasks and acknowledge the suspend event. If
    a thread does not acknowledge the event
    then the driver's suspend routine returns
    failure. But there is a race condition
    where the driver's suspend routine times
    out just before the thread can acknowledge
    the driver and so the thread suspends but the
    driver returns failure to the kernel and since
    the kernel will not call the resume event
    again for a failed suspend, the Thread will
    remain suspended forever.
    
    Fix the race condition by using the APIs
    which will test for the event mask and set
    it atomically.
    
    Change-Id: I81ebbceb7623f6dd09d00c6a79a4913a19dc6e89
    CRs-Fixed: 673980

--- CORE/VOSS/src/vos_sched.c
+++ CORE/VOSS/src/vos_sched.c
@@ -416,10 +416,9 @@ VosMCThread
                 "%s: MC thread signaled to shutdown", __func__);
         shutdown = VOS_TRUE;
         /* Check for any Suspend Indication */
-        if(test_bit(MC_SUSPEND_EVENT_MASK, &pSchedContext->mcEventFlag))
+        if (test_and_clear_bit(MC_SUSPEND_EVENT_MASK,
+                               &pSchedContext->mcEventFlag))
         {
-           clear_bit(MC_SUSPEND_EVENT_MASK, &pSchedContext->mcEventFlag);
-        
            /* Unblock anyone waiting on suspend */
            complete(&pHddCtx->mc_sus_event_var);
         }
@@ -613,9 +612,9 @@ VosMCThread
         continue;
       }
       /* Check for any Suspend Indication */
-      if(test_bit(MC_SUSPEND_EVENT_MASK, &pSchedContext->mcEventFlag))
+      if (test_and_clear_bit(MC_SUSPEND_EVENT_MASK,
+                             &pSchedContext->mcEventFlag))
       {
-        clear_bit(MC_SUSPEND_EVENT_MASK, &pSchedContext->mcEventFlag);
         spin_lock(&pSchedContext->McThreadLock);
 
         /* Mc Thread Suspended */
@@ -901,10 +900,9 @@ static int VosTXThread ( void * Arg )
                  "%s: TX thread signaled to shutdown", __func__);
         shutdown = VOS_TRUE;
         /* Check for any Suspend Indication */
-        if(test_bit(TX_SUSPEND_EVENT_MASK, &pSchedContext->txEventFlag))
+        if (test_and_clear_bit(TX_SUSPEND_EVENT_MASK,
+                               &pSchedContext->txEventFlag))
         {
-           clear_bit(TX_SUSPEND_EVENT_MASK, &pSchedContext->txEventFlag);
-        
            /* Unblock anyone waiting on suspend */
            complete(&pHddCtx->tx_sus_event_var);
         }
@@ -995,9 +993,9 @@ static int VosTXThread ( void * Arg )
         continue;
       }
       /* Check for any Suspend Indication */
-      if(test_bit(TX_SUSPEND_EVENT_MASK, &pSchedContext->txEventFlag))
+      if (test_and_clear_bit(TX_SUSPEND_EVENT_MASK,
+                             &pSchedContext->txEventFlag))
       {
-        clear_bit(TX_SUSPEND_EVENT_MASK, &pSchedContext->txEventFlag);
         spin_lock(&pSchedContext->TxThreadLock);
 
         /* Tx Thread Suspended */
@@ -1097,10 +1095,9 @@ static int VosRXThread ( void * Arg )
                  "%s: RX thread signaled to shutdown", __func__);
         shutdown = VOS_TRUE;
         /* Check for any Suspend Indication */
-        if(test_bit(RX_SUSPEND_EVENT_MASK, &pSchedContext->rxEventFlag))
+        if (test_and_clear_bit(RX_SUSPEND_EVENT_MASK,
+                               &pSchedContext->rxEventFlag))
         {
-           clear_bit(RX_SUSPEND_EVENT_MASK, &pSchedContext->rxEventFlag);
-        
            /* Unblock anyone waiting on suspend */
            complete(&pHddCtx->rx_sus_event_var);
         }
@@ -1171,9 +1168,9 @@ static int VosRXThread ( void * Arg )
       }
 
       /* Check for any Suspend Indication */
-      if(test_bit(RX_SUSPEND_EVENT_MASK, &pSchedContext->rxEventFlag))
+      if (test_and_clear_bit(RX_SUSPEND_EVENT_MASK,
+                             &pSchedContext->rxEventFlag))
       {
-        clear_bit(RX_SUSPEND_EVENT_MASK, &pSchedContext->rxEventFlag);
         spin_lock(&pSchedContext->RxThreadLock);
 
         /* Rx Thread Suspended */
