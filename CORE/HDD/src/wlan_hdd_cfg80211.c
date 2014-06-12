b36a47ac79ebfa6e0a4d96eff52b5ae65a79fe12
Agarwal Ashish <ashishka@qti.qualcomm.com>
2014-06-12 15:16:52 +0530

    wlan: Fix for interface up/down handling as required for STA + STA
    Provide the fix for interface up/down handling in the driver. Currently,
    the driver just disables the tx and turns off the carrier which takes care
    of indicating network subsystem that the interface is down. However,
    the request is also required to make sure the underlying Wifi connection
    is successfully cleaned up along with any pending data (tx/rx).
    
    CRs-Fixed: 669602
    Change-Id: I19b3e65dd096514d451de11b812509bd84021e58

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -2966,7 +2966,7 @@ static int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
 
                     if (pP2pAdapter)
                     {
-                        hdd_stop_adapter(pHddCtx, pP2pAdapter);
+                        hdd_stop_adapter(pHddCtx, pP2pAdapter, VOS_TRUE);
                         hdd_deinit_adapter(pHddCtx, pP2pAdapter);
                         hdd_close_adapter(pHddCtx, pP2pAdapter, VOS_TRUE);
                     }
@@ -2978,7 +2978,6 @@ static int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
                     //Fail to Exit BMPS
                     VOS_ASSERT(0);
                 }
-                hdd_stop_adapter( pHddCtx, pAdapter );
 #ifdef FEATURE_WLAN_TDLS
 
                 /* A Mutex Lock is introduced while changing the mode to
@@ -2988,6 +2987,7 @@ static int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
                 mutex_lock(&pHddCtx->tdls_lock);
 #endif
                 //De-init the adapter.
+                hdd_stop_adapter( pHddCtx, pAdapter, VOS_TRUE );
                 hdd_deinit_adapter( pHddCtx, pAdapter );
                 memset(&pAdapter->sessionCtx, 0, sizeof(pAdapter->sessionCtx));
                 pAdapter->device_mode = (type == NL80211_IFTYPE_AP) ?
@@ -3097,7 +3097,6 @@ static int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
            case NL80211_IFTYPE_STATION:
            case NL80211_IFTYPE_P2P_CLIENT:
            case NL80211_IFTYPE_ADHOC:
-                hdd_stop_adapter( pHddCtx, pAdapter );
 #ifdef FEATURE_WLAN_TDLS
 
                 /* A Mutex Lock is introduced while changing the mode to
@@ -3106,6 +3105,7 @@ static int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
                  */
                 mutex_lock(&pHddCtx->tdls_lock);
 #endif
+                hdd_stop_adapter( pHddCtx, pAdapter, VOS_TRUE );
                 hdd_deinit_adapter( pHddCtx, pAdapter );
                 wdev->iftype = type;
                 //Check for sub-string p2p to confirm its a p2p interface
