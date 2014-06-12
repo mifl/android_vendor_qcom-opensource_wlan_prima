129bd84259e9810d9399e995bcbb1409757f953e
Leela Venkata Kiran Kumar Reddy Chirala <kchirala@qca.qualcomm.com>
2014-06-11 17:16:30 -0700

    Fix for out of boundary memory access in driver
    fix the out of boundary memory access in qosmap
    by correcting the array size.
    
    Change-Id: I7358a5870b43d2110c8bd3714771a6a2b4364ae7
    CRs-fixed: 679023

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -2737,7 +2737,7 @@ typedef struct sSirQosMapSet
     tANI_U8      present;
     tANI_U8      num_dscp_exceptions;
     tANI_U8      dscp_exceptions[21][2];
-    tANI_U8      dscp_range[7][2];
+    tANI_U8      dscp_range[8][2];
 } tSirQosMapSet, *tpSirQosMapSet;
 
 //
