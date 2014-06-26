df9afc3f0e04a022feed3298730e296bd63373d4
Katya Nigam <knigam@qti.qualcomm.com>
2014-06-26 19:04:23 +0530

    wlan: Cache frames only for STA whose assoc/reassoc req is send.
    This fix will introduce a variable enableCaching for each
    STA ID which will be used to enable/disable cache for that
    particular STA. Caching will be enabled when assoc/reassoc
    req is being sent while it is disabled after flushing of
    frames for that STA.
    This fix is done to avaoid low resource condition when
    packets are cached at TL.
    Also removing variable ucRxBlocked as it is no more
    required.
    
    Change-Id: Ic8604f70b835cb5dedcd64662bd6a28f0af6bbfa
    CRs-Fixed: 685340

--- CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -2603,6 +2603,9 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
            psessionEntry->peSessionId,
            pMacHdr->fc.subType));
 
+    // enable caching
+    WLANTL_EnableCaching(psessionEntry->staId);
+
     halstatus = halTxFrame( pMac, pPacket, ( tANI_U16 ) (sizeof(tSirMacMgmtHdr) + nPayload),
             HAL_TXRX_FRM_802_11_MGMT,
             ANI_TXDIR_TODS,
@@ -3412,6 +3415,10 @@ limSendReassocReqMgmtFrame(tpAniSirGlobal     pMac,
     MTRACE(macTrace(pMac, TRACE_CODE_TX_MGMT,
            psessionEntry->peSessionId,
            pMacHdr->fc.subType));
+
+    // enable caching
+    WLANTL_EnableCaching(psessionEntry->staId);
+
     halstatus = halTxFrame( pMac, pPacket, ( tANI_U16 ) (sizeof(tSirMacMgmtHdr) + nPayload),
                             HAL_TXRX_FRM_802_11_MGMT,
                             ANI_TXDIR_TODS,
