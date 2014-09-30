caf-backup/quic_la_platform_vendor_qcom-opensource_wlan_prima

wlan: print local BD/PDUs while taking DXE snapshot
The number of local BD/PDUs which are free can give
leads into why the DXE channels are masked and help
to debug data stalls. So print the number of free
BD/PDUs in local memory when taking a DXE snapshot

Change-Id: I65916ddb6481cf58665ecf20f4c7e1b3635a768d
CRs-Fixed: 708904

prima/CORE/DXE/src/wlan_qct_dxe_i.h
@@ -84,6 +84,7 @@ when           who        what, where, why
 #define WLANDXE_CCU_DXE_INT_SELECT_STAT   0x2050e0
 #define WLANDXE_CCU_ASIC_INT_ENABLE       0x2050e4
 #define WLANDXE_CCU_SOFT_RESET            0x204010
+#define WLANDXE_BMU_AVAILABLE_BD_PDU_LOCAL 0x80260
 #else
 #define WLANDXE_CCU_DXE_INT_SELECT        0x200b10
 #define WLANDXE_CCU_DXE_INT_SELECT_STAT   0x200b14

mifl/android_vendor_qcom-opensource_wlan_prima

CORE/DXE/src/wlan_qct_dxe_i.h
@@ -102,6 +102,7 @@ when           who        what, where, why
 #define WLANDXE_CCU_DXE_INT_SELECT_STAT  0xfb2050e0
 #define WLANDXE_CCU_ASIC_INT_ENABLE      0xfb2050e4
 #define WLANDXE_CCU_SOFT_RESET           0xfb204010
+#define WLANDXE_BMU_AVAILABLE_BD_PDU_LOCAL 0xfb080260
 #else
 #define WLANDXE_CCU_DXE_INT_SELECT       0x03200b10
 #define WLANDXE_CCU_DXE_INT_SELECT_STAT  0x03200b14