caf-backup/quic_la_platform_vendor_qcom-opensource_wlan_prima

wlan: Add check for adapter MAGIC to avoid crash in rx_packet_cbk
hdd_softap_rx_packet_cbk and hdd_rx_packet_cbk do not have check
for adapter MAGIC, Added the same.

Added mactrace in limProcessMlmRspMessages.

Also if LIM receive failure in addsta response for Reassoc request,
lim is posting the message as LIM_MLM_ASSOC_CNF instead of
LIM_MLM_REASSOC_CNF, which will not allow csr to inidcated to HDD.
Added a check to send LIM_MLM_REASSOC_CNF in case of addsta failure
for reassoc request.

CRs-Fixed: 734426

Change-Id: I763cd42ae0e6aa4aea5c9be7c6c0b1d196767d7a

prima/CORE/HDD/src/wlan_hdd_softap_tx_rx.c
@@ -1410,8 +1410,10 @@ VOS_STATUS hdd_softap_rx_packet_cbk( v_VOID_t *vosContext,
    }
 
    pAdapter = pHddCtx->sta_to_adapter[staId];
-   if( NULL == pAdapter )
+   if( (NULL == pAdapter) || (WLAN_HDD_ADAPTER_MAGIC != pAdapter->magic) )
    {
+      VOS_TRACE(VOS_MODULE_ID_HDD_SAP_DATA, VOS_TRACE_LEVEL_ERROR,
+          "%s: invalid adapter or adapter has invalid magic",__func__);
       VOS_ASSERT(0);
       return VOS_STATUS_E_FAILURE;
    }

prima/CORE/HDD/src/wlan_hdd_tx_rx.c
@@ -1866,9 +1866,10 @@ VOS_STATUS hdd_rx_packet_cbk( v_VOID_t *vosContext,
    }
 
    pAdapter = pHddCtx->sta_to_adapter[staId];
-   if( NULL == pAdapter )
+   if( (NULL == pAdapter)  || (WLAN_HDD_ADAPTER_MAGIC != pAdapter->magic) )
    {
-      VOS_TRACE( VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_ERROR,"%s: pAdapter is Null for staId %u",
+      VOS_TRACE( VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_ERROR,
+              "%s: pAdapter is Null or adapter has invalid magic for staId %u",
                  __func__, staId);
       return VOS_STATUS_E_FAILURE;
    }

mifl/android_vendor_qcom-opensource_wlan_prima

CORE/HDD/src/wlan_hdd_softap_tx_rx.c
@@ -1313,8 +1313,10 @@ VOS_STATUS hdd_softap_rx_packet_cbk( v_VOID_t *vosContext,
    }
 
    pAdapter = pHddCtx->sta_to_adapter[staId];
-   if( NULL == pAdapter )
+   if( (NULL == pAdapter) || (WLAN_HDD_ADAPTER_MAGIC != pAdapter->magic) )
    {
+      VOS_TRACE(VOS_MODULE_ID_HDD_SOFTAP, VOS_TRACE_LEVEL_ERROR,
+          "%s: invalid adapter or adapter has invalid magic",__func__);
       VOS_ASSERT(0);
       return VOS_STATUS_E_FAILURE;
    }

CORE/HDD/src/wlan_hdd_tx_rx.c
@@ -1388,9 +1388,10 @@ VOS_STATUS hdd_rx_packet_cbk( v_VOID_t *vosContext,
    }
 
    pAdapter = pHddCtx->sta_to_adapter[staId];
-   if( NULL == pAdapter )
+   if( (NULL == pAdapter)  || (WLAN_HDD_ADAPTER_MAGIC != pAdapter->magic) )
    {
-      VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,"%s: pAdapter is Null for staId %u",
+      VOS_TRACE( VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+               "%s: pAdapter is Null or adapter has invalid magic for staId %u",
                  __func__, staId);
       return VOS_STATUS_E_FAILURE;
    }