f7ef1d819c9e14abeeb4c8fa224929320fc7d044
Abhishek Singh <absingh@qti.qualcomm.com>
2015-03-31 14:18:26 +0530

    wlan: Fix Static analysis error.
    In limProcessSmeGetAssocSTAsInfo() and limProcessSmeGetWPSPBCSessions()
    the pointers pAssocStasTemp and pSapEventCallback respectively are
    dereferenced without checking for NULL.
    
    Added the NULL check in both place.
    
    CRs-Fixed: 815634
    Change-Id: I02c49ff2d7430ed4ade8ca932c73a49802a5b4a4

--- CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -3434,7 +3434,7 @@ void limProcessSmeGetAssocSTAsInfo(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
     {
         pStaDs = dphGetHashEntry(pMac, assocId, &psessionEntry->dph.dphHashTable);
 
-        if (NULL == pStaDs)
+        if ((NULL == pStaDs) || (NULL == pAssocStasTemp))
             continue;
 
         if (pStaDs->valid)
@@ -3555,7 +3555,9 @@ void limProcessSmeGetWPSPBCSessions(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
   
 limGetWPSPBCSessionsEnd:
     pSapEventCallback   = (tpWLAN_SAPEventCB)GetWPSPBCSessionsReq.pSapEventCallback;
-    pSapEventCallback(&sapEvent, GetWPSPBCSessionsReq.pUsrContext);
+
+    if (NULL != pSapEventCallback)
+      pSapEventCallback(&sapEvent, GetWPSPBCSessionsReq.pUsrContext);
 }
 
 
