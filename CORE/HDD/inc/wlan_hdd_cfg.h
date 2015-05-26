2b2c706edfc5f58f2a3a86ea7f1abe1a6a0445f9
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-05-26 15:21:53 +0530

    wlan: Toggle ARP packets BD rates and send using TID 3.
    To make sure AP receives ARP packets, sending ARP packets on BD
    rates 1(1Mbps) and 3(6Mbps) alternatively and for reliability
    sending on TID 3.
    This support will be enabled when ini parameter, gToggleArpBDRates
    is set to TRUE. By default ini parameter will be set to FALSE.
    
    Change-Id: I858c696f9299c37d564cc3e04ea813081e79acad
    CRs-Fixed: 843313

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2450,6 +2450,11 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_DXE_SSR_ENABLE_MIN       ( 0 )
 #define CFG_DXE_SSR_ENABLE_MAX       ( 1 )
 
+#define CFG_TOGGLE_ARP_BDRATES_NAME       "gToggleArpBDRates"
+#define CFG_TOGGLE_ARP_BDRATES_MIN         0
+#define CFG_TOGGLE_ARP_BDRATES_MAX         1
+#define CFG_TOGGLE_ARP_BDRATES_DEFAULT     0
+
 /*--------------------------------------------------------------------------- 
   Type declarations
   -------------------------------------------------------------------------*/ 
@@ -2951,6 +2956,7 @@ typedef struct
    v_U32_t                     dxeReplenishRXTimerVal;
    v_U32_t                     dxeSSREnable;
    v_U8_t                      multicast_host_msgs;
+   v_BOOL_t                    toggleArpBDRates;
 } hdd_config_t;
 /*--------------------------------------------------------------------------- 
   Function declarations and documenation
