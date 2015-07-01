caf-backup/quic_la_platform_vendor_qcom-opensource_wlan_prima

wlan: Excessive logging during tx timeout in softAP.
During hdd_tx_timeout, we print information of all the
queues and all the stations which results into excessive
logging. Reducing the logs to print only those  queues
which has any packet in case of SoftAP mode.

Change-Id: I3b915bf186fe47223744d60ddc3a0cc40055859c
CRs-Fixed: 857264

prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -7127,12 +7127,16 @@ void hdd_wmm_tx_snapshot(hdd_adapter_t *pAdapter)
              hddLog(LOGE, "******STAIndex: %d*********", i);
              for ( j=0; j< NUM_TX_QUEUES; j++)
              {
-                spin_lock_bh(&pSapCtx->aStaInfo[i].wmm_tx_queue[j].lock);
-                hddLog(LOGE, "HDD TxQueue Info For AC: %d Count: %d PrevAdress:%p, NextAddress:%p",
-                       j, pSapCtx->aStaInfo[i].wmm_tx_queue[j].count,
-                       pSapCtx->aStaInfo[i].wmm_tx_queue[j].anchor.prev,
-                       pSapCtx->aStaInfo[i].wmm_tx_queue[j].anchor.next);
-                spin_unlock_bh(&pSapCtx->aStaInfo[i].wmm_tx_queue[j].lock);
+                if( pSapCtx->aStaInfo[i].wmm_tx_queue[j].count )
+                {
+                   spin_lock_bh(&pSapCtx->aStaInfo[i].wmm_tx_queue[j].lock);
+                   hddLog(LOGE, "HDD TxQueue Info For AC: %d Count: %d"
+                         "PrevAdress:%p, NextAddress:%p",
+                         j, pSapCtx->aStaInfo[i].wmm_tx_queue[j].count,
+                         pSapCtx->aStaInfo[i].wmm_tx_queue[j].anchor.prev,
+                         pSapCtx->aStaInfo[i].wmm_tx_queue[j].anchor.next);
+                  spin_unlock_bh(&pSapCtx->aStaInfo[i].wmm_tx_queue[j].lock);
+                }
              }
         }
     }

mifl/android_vendor_qcom-opensource_wlan_prima

CORE/HDD/src/wlan_hdd_wext.c
@@ -5983,12 +5983,16 @@ void hdd_wmm_tx_snapshot(hdd_adapter_t *pAdapter)
              hddLog(LOGE, "******STAIndex: %d*********", i);
              for ( j=0; j< NUM_TX_QUEUES; j++)
              {
-                spin_lock_bh(&pAdapter->aStaInfo[i].wmm_tx_queue[j].lock);
-                hddLog(LOGE, "HDD TxQueue Info For AC: %d Count: %d PrevAdress:%p, NextAddress:%p",
-                       j, pAdapter->aStaInfo[i].wmm_tx_queue[j].count,
-                       pAdapter->aStaInfo[i].wmm_tx_queue[j].anchor.prev,
-                       pAdapter->aStaInfo[i].wmm_tx_queue[j].anchor.next);
-                spin_unlock_bh(&pAdapter->aStaInfo[i].wmm_tx_queue[j].lock);
+                if( pAdapter->aStaInfo[i].wmm_tx_queue[j].count )
+                {
+                   spin_lock_bh(&pAdapter->aStaInfo[i].wmm_tx_queue[j].lock);
+                   hddLog(LOGE, "HDD TxQueue Info For AC: %d Count: %d"
+                         "PrevAdress:%p, NextAddress:%p",
+                         j, pAdapter->aStaInfo[i].wmm_tx_queue[j].count,
+                         pAdapter->aStaInfo[i].wmm_tx_queue[j].anchor.prev,
+                         pAdapter->aStaInfo[i].wmm_tx_queue[j].anchor.next);
+                  spin_unlock_bh(&pAdapter->aStaInfo[i].wmm_tx_queue[j].lock);
+                }
              }
         }
     }