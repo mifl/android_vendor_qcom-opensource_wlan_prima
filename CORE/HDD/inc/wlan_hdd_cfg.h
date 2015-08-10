979f003fd29d333477fd43588b1d0e4aa6c46af6
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-08-10 16:02:15 +0530

    wlan: update ini parameter gBtcFastWlanConnPref MAX value
    Updating ini parameter gBtcFastWlanConnPref MAX value from 1 to 5.
    
    Change-Id: I575f4b7a39e94dc00f8ff48d11cacd40b5a4f4e8
    CRs-Fixed: 830978

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2194,7 +2194,7 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_BTC_FAST_WLAN_CONN_PREF             "gBtcFastWlanConnPref"
 #define CFG_BTC_FAST_WLAN_CONN_PREF_DEFAULT     ( 1 )
 #define CFG_BTC_FAST_WLAN_CONN_PREF_MIN         ( 0 )
-#define CFG_BTC_FAST_WLAN_CONN_PREF_MAX         ( 1 )
+#define CFG_BTC_FAST_WLAN_CONN_PREF_MAX         ( 5 )
 
 /*
  * Connection related log Enable/Disable.
@@ -2912,7 +2912,7 @@ typedef struct
    v_U32_t                     enableDynamicRAStartRate;
    v_U32_t                     enableRtsCtsHtVht;
    v_U8_t                      btcEnableIndTimerVal;
-   v_BOOL_t                    btcFastWlanConnPref;
+   v_U8_t                      btcFastWlanConnPref;
    v_U16_t                     gP2PListenDeferInterval;
 } hdd_config_t;
 /*--------------------------------------------------------------------------- 
