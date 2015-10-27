2a0bc3987aef4cf3392685d1dd8b459d3e0bc3c9
Arusha Goyal <arushag@codeaurora.org>
2015-10-27 22:55:07 +0530

    wlan:Check priviledge permission before processing QCSAP_IOCTL_SETWPSIE
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing QCSAP_IOCTL_SETWPSIE IOCTL,
    making sure user task has right permission to process the command.
    
    Change-Id: Ie1c945afb0f109892beda66bab25647d70cc62d7
    CRs-Fixed: 930944

--- CORE/HDD/src/wlan_hdd_hostapd.c
+++ CORE/HDD/src/wlan_hdd_hostapd.c
@@ -3522,6 +3522,13 @@ static int __iw_softap_setwpsie(struct net_device *dev,
    int ret = 0;
    ENTER();
 
+   if (!capable(CAP_NET_ADMIN))
+   {
+       VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                 FL("permission check failed"));
+       return -EPERM;
+   }
+
    pHostapdAdapter = (netdev_priv(dev));
    if (NULL == pHostapdAdapter)
    {
