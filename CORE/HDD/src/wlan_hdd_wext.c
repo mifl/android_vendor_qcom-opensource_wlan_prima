36a4f934aca5cc1ab1253e5e2f369b80ee43782f
Arun Khandavalli <akhandav@codeaurora.org>
2015-12-16 19:22:45 +0530

    wlan:Check priviledge permission before processing CLEAR_MCBC_FILTER IOCTL
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing CLEAR_MCBC_FILTER IOCTL, making
    sure user task has right permission to process the command.
    
    Change-Id: I9b50fcc0eeb1c1eb3493eab573f4421b52f0ea9a
    CRs-Fixed: 930954

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -6651,6 +6651,12 @@ static int __iw_clear_dynamic_mcbc_filter(struct net_device *dev,
     hdd_context_t *pHddCtx = WLAN_HDD_GET_CTX(pAdapter);
     tpSirWlanSetRxpFilters wlanRxpFilterParam;
     hddLog(VOS_TRACE_LEVEL_INFO_HIGH, "%s: ", __func__);
+    if (!capable(CAP_NET_ADMIN))
+    {
+        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+           FL("permission check failed"));
+        return -EPERM;
+    }
 
     //Reset the filter to INI value as we have to clear the dynamic filter
     pHddCtx->configuredMcastBcastFilter = pHddCtx->cfg_ini->mcastBcastFilterSetting;
