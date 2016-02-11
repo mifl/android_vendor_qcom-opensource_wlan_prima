3c1c6854b80ae7ffaeb5fd6ef8c3ca8f846d994b
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2016-02-11 17:29:27 +0530

    wlan: Resolve static analysis issues
    Resolved  static analysis issues,  variable initialization,
    return on failure, validate data before using.
    
    Change-Id: Ia6ffd422ae3f9bc8419f32aa914839a091841335
    CRs-Fixed: 975049

--- CORE/TL/src/wlan_qct_tl.c
+++ CORE/TL/src/wlan_qct_tl.c
@@ -8428,6 +8428,8 @@ WLANTL_STARxConn
         TLLOGE(VOS_TRACE( VOS_MODULE_ID_TL, VOS_TRACE_LEVEL_ERROR,
                "WLAN TL:Invalid tid  %d (Station ID %d) on %s",
                ucTid, ucSTAId, __func__));
+        vos_pkt_return_packet(vosDataBuff);
+        return VOS_STATUS_E_FAILURE;
       }
 
       TLLOG2(VOS_TRACE( VOS_MODULE_ID_TL, VOS_TRACE_LEVEL_INFO_HIGH,
@@ -8830,6 +8832,8 @@ WLANTL_STARxAuth
     TLLOGE(VOS_TRACE( VOS_MODULE_ID_TL, VOS_TRACE_LEVEL_ERROR,
            "WLAN TL:Invalid tid  %d (Station ID %d) on %s",
            ucTid, ucSTAId, __func__));
+    vos_pkt_return_packet(vosDataBuff);
+    return VOS_STATUS_E_FAILURE;
   }
 
   /*------------------------------------------------------------------------
