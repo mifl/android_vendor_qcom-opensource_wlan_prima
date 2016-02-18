caf-backup/quic_la_platform_vendor_qcom-opensource_wlan_prima

wlan: set wificonfiguration(wfc) parameters to firmware
Program the wifi configurations in the firmware using the vendor
commands

We can set the following three parameters using wfc:

* Modulated DTIM
* Average Stats Factor
* Guard Time

* Existing infrastucture is used for setting the modulated DTIM,
  We check if we are presently in BMPS then comeout of BMPS,
  set the requested modulated DTIM and enter BMPS again. If we are
  not in BMPS state, the requested value shall be effective when we
  enter the BMPS state again.

* For Guard Time & Average stats factor new WDI command is sent to
  firmware(WDI_WIFI_CONFIG_REQ) and the status of the response is
  checked in WDI response callback and the message shall be dropped
  in WDA.

Change-Id: I9ebd1daacdb06e88b064936efe86af78c7fbdac4
CRs-Fixed: 924853

prima/CORE/WDI/CP/inc/wlan_qct_wdi.h
@@ -6179,6 +6179,13 @@ typedef struct
 
 }WDI_MonStartReqType;
 
+typedef struct
+{
+   wpt_uint8  paramType;
+   wpt_uint32 paramValue;
+   wpt_macAddr  bssId;
+}WDI_WifiConfigSetReqType;
+
 /**
  * struct WDI_FwrMemDumpReqType - firmware memory dump request details.
 .*.@FWMemDumpReqCb - Associated Callback
@@ -6204,6 +6211,10 @@ typedef struct
     wpt_uint32 dump_status;
 }WDI_FwrMemDumpRsp;
 
+typedef struct
+{
+    wpt_uint32 wificonfigset_status;
+}WDI_WifconfigSetRsp;
 
 /*----------------------------------------------------------------------------
  *   WDI callback types
@@ -8149,6 +8160,7 @@ typedef void  (*WDI_RssiMonitorStopRspCb)(void *pEventData,void *pUserData);
 
 typedef void  (*WDI_FwrMemDumpRspCb)(WDI_FwrMemDumpRsp *wdiRsp, void *pUserData);
 
+typedef void  (*WDI_WifiConfigSetRspCb) (WDI_WifconfigSetRsp *wdiRsp, void *pUserData);
 
 /*========================================================================
  *     Function Declarations and Documentation
@@ -11777,5 +11789,21 @@ wpt_uint32 val
 #ifdef __cplusplus
  }
 #endif 
+
+/**
+ @brief WDI_WifiConfigSetReq
+    This API is called to send WifiConfig  request to FW
+
+ @param pwdiWifConfigSetReqParams : pointer to  wificonfig params
+        wdiLLStatsSetRspCb     : set wificonfig response callback
+        usrData : Client context
+ @see
+ @return SUCCESS or FAIL
+*/
+WDI_Status
+WDI_WifiConfigSetReq
+   (WDI_WifiConfigSetReqType* pwdiWifConfigSetReqParams,
+    WDI_WifiConfigSetRspCb wdiWifiConfigSetRspCb,
+    void*                 pUserData);
 
 #endif /* #ifndef WLAN_QCT_WDI_H */

mifl/android_vendor_qcom-opensource_wlan_prima

CORE/WDI/CP/inc/wlan_qct_wdi.h
@@ -6142,6 +6142,18 @@ typedef struct
 
 }WDI_MonStartReqType;
 
+typedef struct
+{
+   wpt_uint8  paramType;
+   wpt_uint32 paramValue;
+   wpt_macAddr  bssId;
+}WDI_WifiConfigSetReqType;
+
+typedef struct
+{
+    wpt_uint32 wificonfigset_status;
+}WDI_WifconfigSetRsp;
+
 /*----------------------------------------------------------------------------
  *   WDI callback types
  *--------------------------------------------------------------------------*/
@@ -8065,6 +8077,7 @@ typedef void  (*WDI_MonModeRspCb)(void *pEventData,void *pUserData);
 
 typedef void (*WDI_AntennaDivSelRspCb)(WDI_Status status,
               void *resp, void *pUserData);
+typedef void  (*WDI_WifiConfigSetRspCb) (WDI_WifconfigSetRsp *wdiRsp, void *pUserData);
 
 /*========================================================================
  *     Function Declarations and Documentation
@@ -11627,5 +11640,20 @@ WDI_GetCurrentAntennaIndex
   wpt_uint32 reserved
 );
 
+/**
+ @brief WDI_WifiConfigSetReq
+    This API is called to send WifiConfig  request to FW
+
+ @param pwdiWifConfigSetReqParams : pointer to  wificonfig params
+        WDI_WifiConfigSetRspCb    : set wificonfig response callback
+        usrData : Client context
+ @see
+ @return SUCCESS or FAIL
+*/
+WDI_Status
+WDI_WifiConfigSetReq
+   (WDI_WifiConfigSetReqType* pwdiWifConfigSetReqParams,
+    WDI_WifiConfigSetRspCb wdiWifiConfigSetRspCb,
+    void*                 pUserData);
 
 #endif /* #ifndef WLAN_QCT_WDI_H */