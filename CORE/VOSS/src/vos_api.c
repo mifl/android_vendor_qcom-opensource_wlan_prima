4de3c6e7d79e74093ea77b84d9b8f5e7bbadff0a
Mahesh A Saptasagar <msapta@codeaurora.org>
2016-02-24 16:22:45 +0530

    wlan: Pass proper values to bit manipulation methods to avoid panic
    Observed kernel panic due to improper arguments passed to kernel bit
    manipulation functions (like set_bit, clear_bit etc.) i.e these
    functions expects bit positions as its first argument but bit mask
    values are being passed.
    
    To fix these issues ensure below points:
       - Pass bit position as a first argument to bit manipulation
         functions.
       - Re-define MACROs which gives false impression of bit mask values
         with their naming convention.
    
    Change-Id: Ief8cd83b05f01a0926f91c0e9fb461ddd498e05e
    CRs-Fixed: 981050

--- CORE/VOSS/src/vos_api.c
+++ CORE/VOSS/src/vos_api.c
@@ -1987,13 +1987,12 @@ VOS_STATUS vos_mq_post_message( VOS_MQ_ID msgQueueId, vos_msg_t *pMsg )
 
   vos_mq_put(pTargetMq, pMsgWrapper);
 
-  set_bit(MC_POST_EVENT_MASK, &gpVosContext->vosSched.mcEventFlag);
+  set_bit(MC_POST_EVENT, &gpVosContext->vosSched.mcEventFlag);
   wake_up_interruptible(&gpVosContext->vosSched.mcWaitQueue);
 
   return VOS_STATUS_SUCCESS;
 
 } /* vos_mq_post_message()*/
-
 
 /**---------------------------------------------------------------------------
   
@@ -2105,7 +2104,7 @@ VOS_STATUS vos_tx_mq_serialize( VOS_MQ_ID msgQueueId, vos_msg_t *pMsg )
 
   vos_mq_put(pTargetMq, pMsgWrapper);
 
-  set_bit(TX_POST_EVENT_MASK, &gpVosContext->vosSched.txEventFlag);
+  set_bit(TX_POST_EVENT, &gpVosContext->vosSched.txEventFlag);
   wake_up_interruptible(&gpVosContext->vosSched.txWaitQueue);
 
   return VOS_STATUS_SUCCESS;
@@ -2220,7 +2219,7 @@ VOS_STATUS vos_rx_mq_serialize( VOS_MQ_ID msgQueueId, vos_msg_t *pMsg )
 
   vos_mq_put(pTargetMq, pMsgWrapper);
 
-  set_bit(RX_POST_EVENT_MASK, &gpVosContext->vosSched.rxEventFlag);
+  set_bit(RX_POST_EVENT, &gpVosContext->vosSched.rxEventFlag);
   wake_up_interruptible(&gpVosContext->vosSched.rxWaitQueue);
 
   return VOS_STATUS_SUCCESS;
