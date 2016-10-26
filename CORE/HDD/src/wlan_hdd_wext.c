d8073ff4b95b8348767285215de2741432239a65
Agrawal Ashish <ashishka@qti.qualcomm.com>
2016-10-26 12:41:13 +0530

    wlan: check for SSR in wlan_hdd_set_filter function
    wlan_hdd_set_filter is not checking if SSR is in progress or not.
    fix this by protecting wlan_hdd_set_filter with SSR check.
    
    Change-Id: I58eb632b9ed100dd73b27983035f6f459bb34852
    CRs-Fixed: 1080754

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -8964,6 +8964,12 @@ int wlan_hdd_set_filter(hdd_adapter_t *pAdapter, tpPacketFilterCfg pRequest)
                 __func__ );
         return 0;
     }
+    if (pHddCtx->isLogpInProgress)
+    {
+        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                "%s:LOGP in Progress. Ignore!!!", __func__);
+        return -EBUSY;
+    }
     /* Debug display of request components. */
     hddLog(VOS_TRACE_LEVEL_ERROR, "%s: Packet Filter Request : FA %d params %d",
             __func__, pRequest->filterAction, pRequest->numParams);
