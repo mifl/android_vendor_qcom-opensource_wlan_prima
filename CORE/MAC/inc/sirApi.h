da8a64d371640832418ca0b812d7dffb9dbe3b58
Hanumanth Reddy Pothula <c_hpothu@codeaurora.org>
2016-11-01 16:16:58 +0530

    prima: Wait for clear filter request's response from firmware
    Presently, after sending clear filter request to firmware,
    Host is not waiting for response, leading DHCP packets
    to be dropped at firmware level, if DHCP packets are sent
    before processing clear filter request.
    
    After sending clear filter request, wait for response from
    firmware
    
    Change-Id: Idd51d60357a5f3a9ae5dea81f188a8f403ea0ea5
    CRs-Fixed: 1083987

--- CORE/MAC/inc/sirApi.h
+++ CORE/MAC/inc/sirApi.h
@@ -3745,6 +3745,7 @@ typedef struct sSirWlanSetRxpFilters
 
 typedef void(*FWLoggingInitReqCb)(void *fwlogInitCbContext, VOS_STATUS status);
 typedef void ( *tGetFrameLogCallback) (void *pContext);
+typedef void(*pktFilterReqCb)(void *data, tANI_U32 status);
 
 typedef struct sAniGetFrameLogReq
 {
@@ -4140,6 +4141,8 @@ typedef struct sSirRcvFltPktClearParam
   tANI_U8    filterId;
   tSirMacAddr selfMacAddr;
   tSirMacAddr bssId;
+  pktFilterReqCb     pktFilterCallback;
+  void        *cbCtx;
 }tSirRcvFltPktClearParam, *tpSirRcvFltPktClearParam;
 
 //
