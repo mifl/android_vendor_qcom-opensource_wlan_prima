3d1c68679511c386d18e915774c90a5a6329cb2a
lifeng <lifeng@codeaurora.org>
2019-05-27 12:40:55 +0530

    wlan: Fix possible buffer overflow in sirConvertAddtsRsp2Struct
    In the function sirConvertAddtsRsp2Struct, iterator j is
    assigned with the value pAddTs->numTclas + addts.num_WMMTCLAS.
    The j value is used as the index to the array pAddTs->tclasInfo.
    Maximum limit on  pAddTs->tclasInfo entries is 2. So when the
    value of j exceeds 2, then a possible buffer overflow could
    occur.
    
    Validate the value of j against SIR_MAC_TCLASIE_MAXNUM(2).
    
    Change-Id: Icc723380ed4ccd51c729194d509e288be0e0712c
    CRs-Fixed: 2449899

--- CORE/SYS/legacy/src/utils/src/parserApi.c
+++ CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -4028,7 +4028,7 @@ sirConvertAddtsReq2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
@@ -4210,7 +4210,7 @@ sirConvertAddtsRsp2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
